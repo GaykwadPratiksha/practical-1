83cd413276879b01b65324bfc39d5726a09fc28b
Gaykwad Pratiksha <gaykwad0102@gmail.com>
2022-10-05 14:21:23 +0530

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(args:Array<String>){
+
+    val value1:Int = 15
+    val value2: Float = 2.5f
+    val value3 :Char= 'T'
+    val value4 :String ="Gaykwad Pratiksha"
+    val value5 :Boolean = true
+    val value6:Double = 88.89
+    val value7 :Long = 38299944
+    val value8:Short = -2
+    val value9 :Byte = 127
+
+
+    println("Integer Value : " +value1)
+    println("Float Value : " +value2)
+    println("Character value : " +value3)
+    println("String value : " +value4)
+    println("Boolean value : " +value5)
+    println("Double value : " +value6)
+    println("Long value : " +value7)
+    println("Short value : " +value8)
+    println("Byte value :" +value9)
+
+}
